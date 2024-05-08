8d4ce30e0439b3a84d305a2e5963ba69fae5ef98
Mostakimw <scientist.expert@gmail.com>
2024-05-09 00:18:34 +0600

    cors link changed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["https://unity-aid.vercel.app"], credentials: true }));
 app.use(express.json());
 
 // MongoDB Connection URL
@@ -175,7 +175,7 @@ async function run() {
           { $set: donationData },
           { new: true }
         );
-        
+
         res.status(201).json({
           success: true,
           message: "Donation data fetched successfully",
